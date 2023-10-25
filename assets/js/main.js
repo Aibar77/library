ce4dff1e1c1dd2b587b3eefcc53d5d8603cfa530
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-10-25 16:46:53 +0600

    bug: slider changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,11 +18,59 @@ burgerFilter.addEventListener("click", () => {
   burgerFilter.classList.remove("active");
 });
 
-for (let i = 0; i < navLink.length; i++) {
-  navLink[i].addEventListener("click", () => {
-    burgerMenu.classList.remove("active");
-    burgerBtnOpen.classList.remove("active");
-    body.classList.remove("active");
-    burgerFilter.classList.remove("active");
+burgerMenu.addEventListener("click", (e) => {
+  let link = e.target.closest("a");
+  if (!link) return;
+  burgerMenu.classList.remove("active");
+  burgerBtnOpen.classList.remove("active");
+  body.classList.remove("active");
+  burgerFilter.classList.remove("active");
+});
+
+const paginationDiv = document.querySelector(".pagination");
+const slider = document.querySelector(".carousel-inner");
+const slides = slider.querySelectorAll("img");
+// next dot switch image
+const dots = Array.from(paginationDiv.querySelectorAll(".btn"));
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+let currentSlide = 0;
+let maxSlide = slides.length;
+
+nextBtn.addEventListener("click", (e) => {
+  // console.log(slides[currentSlide + 1].dataset.num);
+
+  if (slides[currentSlide + 1]) {
+    const translateX =
+      -(Number(slides[currentSlide + 1].dataset.num) - 1) * 475;
+    slider.style.transform = `translateX(${translateX}px)`;
+    dots
+      .find((el) => el.classList.contains("current"))
+      .classList.remove("current");
+    dots[currentSlide + 1].classList.add("current");
+    currentSlide++;
+  }
+});
+prevBtn.addEventListener("click", (e) => {
+  if (slides[currentSlide - 1]) {
+    const translateX =
+      -(Number(slides[currentSlide - 1].dataset.num) - 1) * 475;
+    slider.style.transform = `translateX(${translateX}px)`;
+    dots
+      .find((el) => el.classList.contains("current"))
+      .classList.remove("current");
+    dots[currentSlide - 1].classList.add("current");
+    currentSlide--;
+  }
+});
+dots.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    const translateX = -(Number(e.target.dataset.num) - 1) * 475;
+    slider.style.transform = `translateX(${translateX}px)`;
+    currentSlide = Number(e.target.dataset.num) - 1;
+    dots
+      .find((el) => el.classList.contains("current"))
+      .classList.remove("current");
+    e.target.classList.add("current");
   });
-}
+});
