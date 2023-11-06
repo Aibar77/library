89ae0c9570dff9e20febc0c0ba490c86dbe12149
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-11-06 21:35:36 +0600

    feat: add some features

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,13 @@ const burgerMenu = document.querySelector(".nav");
 const body = document.querySelector("body");
 const burgerFilter = document.querySelector(".burger-filter");
 const navLink = document.querySelectorAll(".nav-list-link");
-
+// localStorage.setItem(
+//   "state",
+//   JSON.stringify({
+//     currentUser: null,
+//     state: false,
+//   })
+// );
 burgerBtn.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   burgerBtnOpen.classList.toggle("active");
@@ -20,6 +26,7 @@ burgerFilter.addEventListener("click", () => {
   burgerBtnOpen.classList.remove("active");
   body.classList.remove("active");
   profileMenu.classList.remove("active");
+  registerModal.classList.remove("active");
   burgerFilter.classList.remove("active");
 });
 
@@ -119,4 +126,90 @@ profileIcon.addEventListener("click", (e) => {
   profileMenu.classList.toggle("active");
   burgerMenu.classList.remove("active");
   burgerBtnOpen.classList.remove("active");
+});
+// register modal
+const registerBtn = document.querySelector(".register-btn");
+const registerModal = document.querySelector(".register-modal");
+const registerCloseBtn = document.querySelector("#close_btn");
+const signUpBtn = document.querySelector(".sign-up");
+registerBtn.addEventListener("click", (e) => {
+  profileMenu.classList.remove("active");
+  registerModal.classList.add("active");
+});
+registerCloseBtn.addEventListener("click", (e) => {
+  registerModal.classList.remove("active");
+  burgerFilter.classList.remove("active");
+  body.classList.remove("active");
+});
+signUpBtn.addEventListener("click", (e) => {
+  window.scrollTo({
+    top: 100,
+    behavior: "smooth",
+  });
+  burgerFilter.classList.add("active");
+  registerModal.classList.add("active");
+  body.classList.add("active");
+});
+
+const registerForm = document.querySelector("#register-form");
+
+function generateRandomHexCardNumber() {
+  const cardNumberLength = 9; // Длина номера карты (9 цифр)
+  let cardNumber = "";
+
+  for (let i = 0; i < cardNumberLength; i++) {
+    // Генерируем случайную шестнадцатеричную цифру (0-9 и A-F)
+    const randomHexDigit = Math.floor(Math.random() * 16);
+    cardNumber += randomHexDigit.toString(16).toUpperCase();
+  }
+
+  return cardNumber;
+}
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  registerModal.classList.remove("active");
+  burgerFilter.classList.remove("active");
+  body.classList.remove("active");
+  const fNameInput = registerForm.querySelector("#fname");
+  const lNameInput = registerForm.querySelector("#lname");
+  const emailInput = registerForm.querySelector("#e-mail");
+  const pwsInput = registerForm.querySelector("#password");
+  const cardNumber = generateRandomHexCardNumber();
+  const newUser = {
+    "first-name": fNameInput.value,
+    "last-name": lNameInput.value,
+    email: emailInput.value,
+    password: pwsInput.value,
+    "card-number": cardNumber,
+    visits: 1,
+  };
+
+  authorize(newUser);
+  localStorage.setItem(
+    "state",
+    JSON.stringify({
+      currentUser: newUser,
+      state: true,
+    })
+  );
+  localStorage.setItem(newUser.password, JSON.stringify(newUser));
+});
+// after sign-up state
+function authorize(user) {
+  let isNew = localStorage.getItem(user.password);
+  console.log(JSON.parse(isNew));
+
+  let userIcon = user["first-name"][0] + user["last-name"][0];
+  profileIcon.innerHTML = `<div class='profile-icon authorized' title="${
+    user["first-name"] + " " + user["last-name"]
+  }">${userIcon}</div>`;
+  profileMenu.innerHTML = `<p>${user["card-number"]}</p>
+    <button class="myprofile-btn">My profile</button>
+    <button class="logout-btn">Log Out</button>`;
+}
+window.addEventListener("DOMContentLoaded", (e) => {
+  let state = JSON.parse(localStorage.getItem("state"));
+  if (!!state) {
+    authorize(state.currentUser);
+  }
 });
